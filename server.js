a31af51b09c02b7f3480ae39d2cf8ff53a6a7356
ektravel <38017980+ektravel@users.noreply.github.com>
2018-06-28 16:10:23 -0500

    have server serving

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.static("client/build"));
 
 app.get("/", (req, res) => {
     res.send("hi");
@@ -19,6 +22,10 @@ app.post("/api/test", (req, res) => {
     console.log(req.body);
     req.body.received = true;
     res.json(req.body);
+});
+
+app.use(function(req, res){
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.listen(PORT, function() {
